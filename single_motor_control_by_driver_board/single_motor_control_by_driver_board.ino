d88574fa98e44e2e4d69c6eadd88ad5e309c1781
pstsengb <frasherfrasher01@gmail.com>
2021-12-14 01:33:58 +0800

    signle motor ver3

--- single_motor_control_by_driver_board/single_motor_control_by_driver_board.ino
+++ single_motor_control_by_driver_board/single_motor_control_by_driver_board.ino
@@ -1,24 +1,16 @@
-int B_PWM = 9;
-int B_1 = 10;
-int B_2 = 11;
 int A_PWM = 18 ;
 int A_1 = 14;
 int A_2 = 15; 
 
 
 void setup() {
-  pinMode(B_PWM, OUTPUT);
-  pinMode(B_1, OUTPUT);
-  pinMode(B_2, OUTPUT);
+
   pinMode(A_PWM, OUTPUT);
   pinMode(A_1, OUTPUT);
   pinMode(A_2, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(B_1,LOW);
-  digitalWrite(B_2,HIGH);
-  analogWrite(B_PWM,30);
   digitalWrite(A_1,LOW);
   digitalWrite(A_2,HIGH);
   analogWrite(A_PWM,20);

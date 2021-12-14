935877c29c8d95048068d7ed38a1cab925132453
pstsengb <frasherfrasher01@gmail.com>
2021-12-14 15:04:51 +0800

    ver2

--- motor_control_and_read_by_driver_board/motor_control_and_read_by_driver_board.ino
+++ motor_control_and_read_by_driver_board/motor_control_and_read_by_driver_board.ino
@@ -11,23 +11,68 @@ int A_PWM = 9 ;
 int A_1 = 10;
 int A_2 = 11; 
 
-void setup() {
+void setup(){
   Serial.begin(9600);
   pinMode(A_PWM, OUTPUT);
   pinMode(A_1, OUTPUT);
   pinMode(A_2, OUTPUT);
 }
-
-void loop() {
-  digitalWrite(A_1,LOW);
-  digitalWrite(A_2,HIGH);
-  analogWrite(A_PWM,10);
+void loop(){
   long pluse_1;
   long rpm1;
-  pluse_1=motor1.read();
-  rpm1=(pluse_1*time_s_m)/sensor_R;
+  pluse_1 = motor1.read();
+  rpm1 = (pluse_1*time_s_m)/sensor_R;
   Serial.printf("motor pluse: %ld \n", pluse_1);
   Serial.printf("motor rpm: %ld  \n", rpm1);
   motor1.write(0);
   delay(timeunit);
+  if(Serial.available()>0){
+    if(Serial.available()==3){
+     int A = Serial.read();
+     int B = Serial.read();
+     int C = Serial.read();
+     A = A-48;
+     B = B-48;
+     C = C-48;
+     int OUT = A*100+B*10+C;
+       if(OUT<=255){
+         Serial.printf("OUT:%d\n",OUT);
+         digitalWrite(A_1,LOW);
+         digitalWrite(A_2,HIGH);
+         analogWrite(A_PWM,OUT);
+       }
+    }
+    if(Serial.available()==2){
+     int A = Serial.read();
+     int B = Serial.read();
+     A = A-48;
+     B = B-48;
+     int OUT = A*10+B;
+       if(OUT<=255){
+         Serial.printf("OUT:%d\n",OUT);
+         digitalWrite(A_1,LOW);
+         digitalWrite(A_2,HIGH);
+         analogWrite(A_PWM,OUT);
+       }
+    }
+    if(Serial.available()==1){
+      int A = Serial.read();
+      A = A-48;
+      int OUT = A;
+      if(OUT<=255){
+      Serial.printf("OUT:%d\n",OUT);
+      digitalWrite(A_1,LOW);
+      digitalWrite(A_2,HIGH);
+      analogWrite(A_PWM,OUT);
+      }
+    }
+    else{
+      int serial_length = Serial.available();
+      for(int i=0;i<serial_length;i++){
+        int dummy_reader = Serial.read();
+        Serial.print("dummy_reader:");
+        Serial.println(dummy_reader);
+      }
+    }
+  }
 }
